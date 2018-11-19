64025519cf88a9c52543cdd40e394c5c0acccaa6
Kartik <kartiksinghal122@gmail.com>
2018-11-19 23:12:34 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -29,10 +29,7 @@ $(".sidebar-menu-toggle").click(function(){
              if(!$(".sidebar-menu-toggle").hasClass("show-sidebar")){    // make sure sidebar-menu-toggle-anim class doesn't clash with sidebar-menu-toggle-anim-reverse
                 $(".sidebar-menu-toggle").toggleClass('rotated');
                 $(window).click(function(){
-                    if($(".sidebar-menu-toggle").hasClass("show-sidebar")){
-                        $(".sidebar-menu-toggle").removeAttr("color");
-                        $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass('sidebar-menu-toggle-anim-reverse');
-                    }
+                        $(".sidebar-menu-toggle").toggleClass('rotated');
                 })
              }
              else{
