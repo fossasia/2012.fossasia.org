15caf9dc0af228b91087a81b66fd16623695547a
Kartik <kartiksinghal122@gmail.com>
2018-11-19 21:54:57 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -27,11 +27,13 @@ $('.map-holder').click(function () {
 
 $(".sidebar-menu-toggle").click(function(){    
              if($(".sidebar-menu-toggle").css("marginRight") != "-300px"){
-                $(".sidebar-menu-toggle").removeClass('sidebar-menu-toggle-anim-reverse');       // make sure sidebar-menu-toggle-anim class doesn't clash with sidebar-menu-toggle-anim-reverse
+                $(".sidebar-menu-toggle").removeClass('sidebar-menu-toggle-anim-reverse');  
+                $(".sidebar-menu-toggle").css("color","#fff");    // make sure sidebar-menu-toggle-anim class doesn't clash with sidebar-menu-toggle-anim-reverse
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass('sidebar-menu-toggle-anim');
              }
              else{
                 $(".sidebar-menu-toggle").removeClass('sidebar-menu-toggle-anim');            // make sure sidebar-menu-toggle-anim-reverse class doesn't clash with sidebar-menu-toggle-anim
+                $(".sidebar-menu-toggle").removeAttr("color");  
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass('sidebar-menu-toggle-anim-reverse');
              }
         });
