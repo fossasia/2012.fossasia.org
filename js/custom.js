46789086306e06651152ef8a2ac88ba423a2a011
Kartik <kartiksinghal122@gmail.com>
2018-11-19 23:48:31 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -26,10 +26,10 @@ $('.map-holder').click(function () {
 });
 
 $(".sidebar-menu-toggle").click(function(){    
-             if(!$(".sidebar-menu-toggle").hasClass("show-sidebar")){    // make sure sidebar-menu-toggle-anim class doesn't clash with sidebar-menu-toggle-anim-reverse
+             if(!$(".sidebar-menu").hasClass("show-sidebar")){    // make sure sidebar-menu-toggle-anim class doesn't clash with sidebar-menu-toggle-anim-reverse
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass("sidebar-btn-color");
                 $(window).click(function(){
-                    if($(".sidebar-menu-toggle").hasClass("show-sidebar")){
+                    if($(".sidebar-menu").hasClass("show-sidebar")){
                         $(".sidebar-menu-toggle").toggleClass('rotated');
                         $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
                     }
