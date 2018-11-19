1ad3aca7b26ffbe72315c1a33f5430756a454375
Kartik <kartiksinghal122@gmail.com>
2018-11-19 23:39:59 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -29,6 +29,7 @@ $(".sidebar-menu-toggle").click(function(){
              if(!$(".sidebar-menu-toggle").hasClass("show-sidebar")){    // make sure sidebar-menu-toggle-anim class doesn't clash with sidebar-menu-toggle-anim-reverse
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass("sidebar-btn-color");
                 $(window).click(function(){
+                    debugger
                     if($(".sidebar-menu-toggle").hasClass("show-sidebar")){
                         $(".sidebar-menu-toggle").toggleClass('rotated');
                         $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
