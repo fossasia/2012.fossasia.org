8c1e26db860e7250b3ef4bd1446dbc5de6783cc3
Kartik <kartiksinghal122@gmail.com>
2018-11-20 00:15:28 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -30,11 +30,9 @@ $(".sidebar-menu-toggle").click(function(){
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass("sidebar-btn-color");
                 $(".main-container").click(function(){
                     debugger;
-                    if($(".main-container").hasClass("reveal-sidebar")){
                         $(".sidebar-menu-toggle").toggleClass('rotated');
                         $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
                         $(".main-container").removeClass("reveal-sidebar");
-                    }
                 })
              }
              else{
