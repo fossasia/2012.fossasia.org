ea3c9a89de136af4d382d9b6d03c2f0e2be5e765
Kartik <kartiksinghal122@gmail.com>
2018-11-20 22:15:26 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -24,42 +24,20 @@ $(document).ready(function () {
 $('.map-holder').click(function () {
     $(this).removeClass('zoom-off');
 });
-var position = null ;
-var distance_left = null;;
-var screen_width = null;
+
 $(".sidebar-menu-toggle").click(function(){    
              if(!$(".main-container").hasClass("reveal-sidebar")){    
                 debugger;
-                position = $(".sidebar-menu-toggle").offset();
-                distance_left = position.left;
-                screen_width = $(window).width();
-                margin_left = screen_width - distance_left + 200 ;
-                margin_left = margin_left.toString();
-                margin_left = "-".concat(margin_left,"px");
-                $(".sidebar-menu-toggle").animate({right: margin_left});
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass("sidebar-btn-color");
                 $(".main-container").click(function(){
-                    debugger
-                    $(".sidebar-menu-toggle").animate({right: "0px"});
-                    $(".sidebar-menu-toggle").toggleClass('rotated');
-                    $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
+                        $(".sidebar-menu-toggle").toggleClass('rotated');
+                        $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
+                        $(".main-container").removeClass("reveal-sidebar");
                 });
-                $(window).resize(function(){
-                    position = $(".sidebar-menu-toggle").offset();
-                    distance_left = position.left;
-                    screen_width = $(window).width();
-                    margin_left = screen_width - distance_left + 200 ;
-                    margin_left = margin_left.toString();
-                    margin_left = "-".concat(margin_left,"px");
-                    $(".sidebar-menu-toggle").animate({right: margin_left});
-                });
-
              }
              else{
                 debugger;
-                $(".sidebar-menu-toggle").animate({right: "0px"});
                 $(".sidebar-menu-toggle").toggleClass('rotated');
                 $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
-                $(".main-container").off("click");
              }
         });
