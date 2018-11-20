4f5eef0c4e284743d35cdedc3d4c331893dd541b
Kartik <kartiksinghal122@gmail.com>
2018-11-20 15:43:51 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -24,17 +24,17 @@ $(document).ready(function () {
 $('.map-holder').click(function () {
     $(this).removeClass('zoom-off');
 });
-var myFunc = function (){
-    $(".sidebar-menu-toggle").toggleClass('rotated');
-    $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
-    $(".main-container").removeClass("reveal-sidebar");
-    $(".main-container").removeEventListener("click" , myFunc);
+function myFunc(){
+    $(".main-container").click(function(){
+                        $(".sidebar-menu-toggle").toggleClass('rotated');
+                        $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
+                        $(".main-container").removeClass("reveal-sidebar");
+                        $(".main-container").removeEventListener("click" , myfunc, false);
+                });
 }
 var event = new Event('build');
 var elem = $(".main-container");
-elem.addEventListener('build', function (e) { 
-    $(".main-container").click(myFunc);
- }, false);
+elem.addEventListener('build', myFunc, false);
 $(".sidebar-menu-toggle").click(function(){    
              if(!$(".main-container").hasClass("reveal-sidebar")){    // make sure sidebar-menu-toggle-anim class doesn't clash with sidebar-menu-toggle-anim-reverse
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass("sidebar-btn-color");
@@ -43,5 +43,6 @@ $(".sidebar-menu-toggle").click(function(){
              else{
                 $(".sidebar-menu-toggle").toggleClass('rotated');
                 $(".sidebar-menu-toggle").removeClass("sidebar-btn-color");
+                $(".main-container").removeEventListener(event , myfunc, false);
              }
         });
