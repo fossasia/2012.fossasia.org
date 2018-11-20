537be5456d7bf81eaaaadeb69bb21dabe5301200
Kartik <kartiksinghal122@gmail.com>
2018-11-20 19:10:49 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -33,7 +33,7 @@ $(".sidebar-menu-toggle").click(function(){
                 position = $(".sidebar-menu-toggle").offset();
                 distance_left = position.left;
                 screen_width = $(window).width();
-                margin_left = 200;
+                margin_left = "200px";
                 $(".sidebar-menu-toggle").animate({marginLeft: margin_left});
                 $(".sidebar-menu-toggle").toggleClass('rotated').toggleClass("sidebar-btn-color");
                 $(".main-container").click(function(){
