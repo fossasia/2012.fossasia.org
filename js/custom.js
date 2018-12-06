0b716fe4b4026f60c0895f9d24e25b088bba2999
Saarthak Chaturvedi <saarthakchats05@gmail.com>
2018-12-06 18:04:38 +0530

    dot fixes

--- js/custom.js
+++ js/custom.js
@@ -42,7 +42,7 @@ function readMore() {
 }
 
 function readMore2() {
-  var dots = document.getElementById("dots");
+  var dots = document.getElementById("dots2");
   var moreText = document.getElementById("more2");
   var btnText = document.getElementById("myBtn2");
 
@@ -58,7 +58,7 @@ function readMore2() {
 }
 
 function readMore3() {
-  var dots = document.getElementById("dots");
+  var dots = document.getElementById("dots3");
   var moreText = document.getElementById("more3");
   var btnText = document.getElementById("myBtn3");
 
@@ -74,7 +74,7 @@ function readMore3() {
 }
 
 function readMore4() {
-  var dots = document.getElementById("dots");
+  var dots = document.getElementById("dots4");
   var moreText = document.getElementById("more4");
   var btnText = document.getElementById("myBtn4");
 
